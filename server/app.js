9683cba947fa848a160304718ebc5688b4985501
Hilario Nengare <hnengare@gmail.com>
2023-07-11 22:25:04 +0300

    redo

--- server/app.js
+++ server/app.js
@@ -4,10 +4,11 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const buildPath = path.join(__dirname, 'build');
+const buildPath = path.join(__dirname, '../evbulucu/build');
 
 app.use(express.static(buildPath));
 
+// added buildpath
 app.get('/', (req, res) => {
   res.sendFile(path.join(buildPath, 'index.html'));
 });
