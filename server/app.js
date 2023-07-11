cdc71350b569451ab0ed3b3f03975e48cd433b31
Hilario Nengare <hnengare@gmail.com>
2023-07-11 22:04:31 +0300

    3000

--- server/app.js
+++ server/app.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '../evbulucu/build')));
+app.use(express.static(path.join(__dirname, 'evbulucu', 'build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../evbulucu/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'evbulucu', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
