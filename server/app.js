1da30b0ea7a41946778e141fbc01bde5581bb875
Hilario Nengare <hnengare@gmail.com>
2023-07-15 23:17:33 +0300

    reload fix

--- server/app.js
+++ server/app.js
@@ -6,12 +6,17 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const buildPath = path.join(__dirname, '../evbulucu/build');
+app.use(express.static(path.join(__dirname, '../evbulucu/build')));
 
-app.use(express.static(buildPath));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(buildPath, 'index.html'));
+app.use((req, res, next) => {
+  if (/(\.ico|\.js|\.css|\.jpg|\.png|\.map)$/i.test(req.path)) {
+    next();
+  } else {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    res.sendFile(path.join(__dirname, '../evbulucu/build', 'index.html'));
+  }
 });
 
 app.listen(port, () => {
