ed9bfb6f5e582c87c6eca2938b0e4ef39cba7b1e
Hilario Nengare <hnengare@gmail.com>
2023-07-17 16:53:38 +0300

    Added listing details

--- evbulucu/src/components/CreatePost.js
+++ evbulucu/src/components/CreatePost.js
@@ -2,164 +2,159 @@ import React, { useState } from 'react';
 import './CreatePost.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { Navbar, Container, Form, FormControl, Modal, Nav } from 'react-bootstrap';
 
 const CreatePost = () => {
+  const [imageFiles, setImageFiles] = useState([]);
+  const [address, setAddress] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
 
-    const [imageFiles, setImageFiles] = useState([]);
-    const [details, setDetails] = useState({
-        wifi: false,
-        busStopDistance: '',
-        runningWater: false,
-        market: '',
-        rooms: '',
-        school: '',
-        parking: '',
-    });
+  const [details, setDetails] = useState({
+    wifi: false,
+    busStopDistance: '',
+    runningWater: false,
+    market: '',
+    rooms: '',
+    school: '',
+    parking: '',
+  });
 
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setImageFiles(files);
+  };
 
-    const handleFileChange = (e) => {
-        const files = Array.from(e.target.files);
-        setImageFiles(files);
-    };
+  const handleInputChange = (event) => {
+    const { name, type, value, checked } = event.target;
+    const newValue = type === 'checkbox' ? checked : value;
+    setDetails((prevDetails) => ({
+      ...prevDetails,
+      [name]: newValue,
+    }));
+  };
 
-    const handleInputChange = (event) => {
-        const { name, type, value, checked } = event.target;
-        const newValue = type === 'checkbox' ? checked : value;
-        setDetails((prevDetails) => ({
-            ...prevDetails,
-            [name]: newValue,
-        }));
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Form submission logic goes here
+  };
 
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-    };
-
-    return (
-        <div className='container'>
-          <div className='row p-3 mx-auto'>
-           
-            <div className='col-lg-6 col-12 ms-3'>
-              <form className='upload-form'>
-                <div className='mb-4'>
-                  <h3 className='section-title'>Gallery</h3>
-                  <div className='input-group'>
-                    <div className='custom-file'>
-                      <input
-                        name='images'
-                        className='custom-file-input shadow-none'
-                        type="file"
-                        multiple
-                        onChange={handleFileChange}
-                      />
-                      <label className='custom-file-label' htmlFor='images'>
-                        Upload Images
-                      </label>
-                    </div>
-                  </div>
-                </div>
-                <div className='mb-4'>
-                  <h3 className='section-title'>Address</h3>
-               
-                </div>
-                <div className='mb-4'>
-                  <h3 className='section-title'>Overview</h3>
-                  <div className='form-check'>
-                    <input
-                      className='form-check-input'
-                      type='checkbox'
-                      id='wifi'
-                      name='wifi'
-                      checked={details.wifi}
-                      onChange={handleInputChange}
-                    />
-                    <label className='form-check-label' htmlFor='wifi'>
-                      WiFi
-                    </label>
-                  </div>
-                  <div className='form-check'>
-                    <input
-                      className='form-check-input'
-                      type='checkbox'
-                      id='busStopDistance'
-                      name='busStopDistance'
-                      checked={details.busStopDistance}
-                      onChange={handleInputChange}
-                    />
-                    <label className='form-check-label' htmlFor='busStopDistance'>
-                      Bus Stop Distance
-                    </label>
-                    {/* Input field for bus stop distance */}
-                  </div>
-                  {/* Other overview fields */}
-                </div>
-                <div className='mb-4'>
-                  <h3 className='section-title'>Facilities</h3>
-                  <div className='form-check'>
-                    <input
-                      className='form-check-input'
-                      type='checkbox'
-                      id='running-water'
-                      name='running-water'
-                      checked={details.runningWater}
-                      onChange={handleInputChange}
-                    />
-                    <label className='form-check-label' htmlFor='running-water'>
-                      Running Water
-                    </label>
-                  </div>
-                  <div className='form-check'>
-                    <input
-                      className='form-check-input'
-                      type='checkbox'
-                      id='parking'
-                      name='parking'
-                      checked={details.parking}
-                      onChange={handleInputChange}
-                    />
-                    <label className='form-check-label' htmlFor='parking'>
-                      Parking
-                    </label>
-                  </div>
-                  <div className='form-check'>
-                    <input
-                      className='form-check-input'
-                      type='checkbox'
-                      id='school'
-                      name='school'
-                      checked={details.school}
-                      onChange={handleInputChange}
-                    />
-                    <label className='form-check-label' htmlFor='school'>
-                      School
-                    </label>
-                  </div>
-                  <div className='form-check'>
-                    <input
-                      className='form-check-input'
-                      type='checkbox'
-                      id='rooms'
-                      name='rooms'
-                      checked={details.rooms}
-                      onChange={handleInputChange}
-                    />
-                    <label className='form-check-label' htmlFor='rooms'>
-                      Rooms
-                    </label>
-                  </div>
-                </div>
-                <div className='d-flex justify-content-center text-center mb-3'>
-                  <button className='btn btn-primary' type='submit'>Submit</button>
-                </div>
-              </form>
-            </div>
-          </div>
+  return (
+    <div className='container m-1'>
+      <div className='text-center'>
+        <h2 className='m-3 mx-auto text-center'>List Your Property</h2>
+        <p>Reach tons of renters. Get in touch. Screen tenants. Set up rent payments.</p>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group m-2'>
+          <label htmlFor='exampleFormControlSelect1'>Type of property</label>
+          <select className='form-control' id='exampleFormControlSelect1'>
+            <option>Apartment</option>
+            <option>Studio</option>
+            <option>House</option>
+          </select>
         </div>
-      );
-      
+        <div className='form-group m-2'>
+          <label htmlFor='address'>Address</label>
+          <input
+            type='text'
+            className='form-control'
+            id='address'
+            title='Enter property address'
+            aria-describedby='addrInfo'
+            placeholder='123 Street_Name'
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <small id='addrInfo' className='form-text text-muted'>
+            Type address in full to reach more people.
+          </small>
+        </div>
+        <div className='form-group m-2'>
+          <label htmlFor='beds'>Number of <strong>beds</strong></label>
+          <select className='form-control' id='beds'>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+          </select>
+        </div>
+        <div className='form-group m-2'>
+          <label htmlFor='baths'>Number of <strong>baths</strong></label>
+          <select className='form-control' id='baths'>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+          </select>
+        </div>
+        <div className='form-group m-2'>
+          <label htmlFor='rooms'>Number of <strong>rooms</strong></label>
+          <select className='form-control' id='rooms'>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+          </select>
+        </div>
+        <div className='form-check'>
+          <input
+            type='checkbox'
+            className='form-check-input'
+            id='exampleCheck1'
+            checked={isChecked}
+            onChange={(e) => setIsChecked(e.target.checked)}
+          />
+          <label className='form-check-label' htmlFor='exampleCheck1'>
+            Check me out
+          </label>
+        </div>
+        <div className='form-check'>
+          <input
+            type='checkbox'
+            className='form-check-input'
+            id='exampleCheck1'
+            checked={isChecked}
+            onChange={(e) => setIsChecked(e.target.checked)}
+          />
+          <label className='form-check-label' htmlFor='exampleCheck1'>
+            Check me out
+          </label>
+        </div>
+        <div className='form-check'>
+          <input
+            type='checkbox'
+            className='form-check-input'
+            id='exampleCheck1'
+            checked={isChecked}
+            onChange={(e) => setIsChecked(e.target.checked)}
+          />
+          <label className='form-check-label' htmlFor='exampleCheck1'>
+            Check me out
+          </label>
+        </div>
+        <div className='form-check'>
+          <input
+            type='checkbox'
+            className='form-check-input'
+            id='exampleCheck1'
+            checked={isChecked}
+            onChange={(e) => setIsChecked(e.target.checked)}
+          />
+          <label className='form-check-label' htmlFor='exampleCheck1'>
+            Check me out
+          </label>
+        </div>
+        <button type='submit' className='btn btn-primary mt-2 mx-auto d-flex justify-content-center'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CreatePost;
