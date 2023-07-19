8f463cb34077aa5f26758680ab1a467596fe14eb
Hilario Nengare <hnengare@gmail.com>
2023-07-19 13:31:48 +0300

    everything is new

--- server/routes.js
+++ server/routes.js
@@ -5,35 +5,36 @@ const db = require('./db');
 const router = express.Router();
 
 // register route
-app.post('/register', async(req, res)=>{
-    try{
+app.post('/register', async (req, res) => {
+    try {
         const user = req.body;
         const result = await db.save(user);
-        res.json({success: true, id: result.id});
-    }catch (error){
-        res.status(500).json({success: false, error: error.message });
+        res.json({ success: true, id: result.id });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
     }
 });
 
 // login route
-app.post('/login', async(req, res)=>{
-    try{
-        const {email, password} = req.body;
-        db.view('users/login-by-email', {key: email}, (err, rows) => {
+app.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        db.view('users/login-by-email', { key: email }, (err, rows) => {
             if (err || rows.length === 0) {
                 return res.status(404).json({ success: false, message: 'User not found' });
-            }else{
+            } else {
                 const user = rows[0].value;
             }
 
-            if (user.password === password){
-                return res.json({success: true, user});
-            }else{
-                
+            if (user.password === password) {
+                return res.json({ success: true, user });
+            } else {
+                res.status(401).json({ success: false, message: 'Invalid credentials' });
             }
-        
         });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
     }
-})
+});
 
 module.exports = router;
