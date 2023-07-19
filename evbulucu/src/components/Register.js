3d706b09ed4ead341b4e01967845240e865d609f
Hilario Nengare <hnengare@gmail.com>
2023-07-19 20:29:17 +0300

    added authentication

--- evbulucu/src/components/Register.js
+++ evbulucu/src/components/Register.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './Register.css';
-import Footer from './Footer';
+import axios from 'axios';
 
 const Register = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/register', {
+        name,
+        email,
+        password,
+      });
+
+      const { success, id, error } = response.data;
+      if (password !== confirmPassword){
+        console.log('Passwords do not match..sorry');
+      }
+
+      if (success) {
+        console.log('Registration successful! User ID:', id);
+      } else {
+        console.log('Registration failed:', error);
+      }
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+    }
+  };
+
   return (
     <div className='container-fluid'>
       <div className='row justify-content-center align-items-center mb-5'>
@@ -16,12 +46,26 @@ const Register = () => {
               <h1 className='h3 font-weight-normal mt-3'>Registration</h1>
             </div>
             <div className='form-group mb-3'>
+              <label htmlFor='inputName'>Name</label>
+              <input
+                type='text'
+                id='inputName'
+                value={name}
+                className='form-control'
+                placeholder='Your full name'
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className='form-group mb-3'>
               <label htmlFor='inputEmail'>Email address</label>
               <input
                 type='email'
                 id='inputEmail'
+                value={email}
                 className='form-control'
                 placeholder='Email address'
+                onChange={(e)=> setEmail(e.target.value)}
                 required
               />
             </div>
@@ -31,8 +75,10 @@ const Register = () => {
               <input
                 type='password'
                 id='userPassword'
+                value={password}
                 className='form-control'
                 placeholder='Password'
+                onChange={(e) => setPassword(e.target.value)}
                 required
               />
             </div>
@@ -42,13 +88,15 @@ const Register = () => {
               <input
                 type='password'
                 id='confirmPassword'
+                value={confirmPassword}
                 className='form-control'
+                onChange={(e) => setConfirmPassword(e.target.value)}
                 placeholder='Confirm Password'
                 required
               />
             </div>
 
-            <button className='btn btn-lg btn-primary btn-block mt-2' type='submit'>
+            <button className='btn btn-lg btn-primary btn-block mt-2' type='submit' onClick={handleRegister}>
               Register
             </button>
           </form>
